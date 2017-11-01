af08f49c3bc0d2173cdc02f2c1a3a271aaee7ade
Dickson Law <dickson.law@utoronto.ca>
2017-11-01 12:21:36 -0400

    Add IIIF Toolkit download to Omeka install routine

--- setup/omeka.sh
+++ setup/omeka.sh
@@ -2,3 +2,4 @@
 # omeka.sh
 
 /home/vagrant/.bash-tools/omekash new main --branch stable-2.5 --repo https://github.com/omeka/Omeka.git
+git clone https://github.com/utlib/IiifItems.git --recursive /var/www/html/omeka-main/plugins/IiifItems
