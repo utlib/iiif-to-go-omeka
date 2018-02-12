cfa59097573d2f9461f5c005762584f51be2718c
Dickson Law <dickson.law@utoronto.ca>
2018-02-12 11:08:21 -0500

    Add exit on fail and open-check for start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-vagrant up
-xdg-open "http://127.0.0.1:8080"
+url="http://127.0.0.1:8080"
+vagrant up || exit 127
+if [ ! -z `which xdg-open` ]; then
+	xdg-open "$url"
+elif [ ! -z `which open` ]; then
+	open "$url"
+fi
