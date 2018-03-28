db0f8a9068a068f6ef796a73177d373b7ef0ed17
Dickson Law <dickson.law@utoronto.ca>
2018-03-28 16:59:49 -0400

    Add zip during initial install

--- setup/boot.sh
+++ setup/boot.sh
@@ -4,4 +4,4 @@ export DEBIAN_FRONTEND=noninteractive
 echo "IIIF in a Box: Initializing setup..."
 
 apt-get update -qqy > /dev/null 2>&1
-apt-get -qqy install wget git unzip imagemagick > /dev/null 2>&1
+apt-get -qqy install wget git zip unzip imagemagick > /dev/null 2>&1
