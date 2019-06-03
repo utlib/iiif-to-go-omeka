dd2352f2069d50da609964d273c7eca1bf396ed2
Dickson Law <dickson.law@utoronto.ca>
2019-06-03 13:22:30 -0400

    Upgrade Loris to 2.3.3

--- setup/loris.sh
+++ setup/loris.sh
@@ -15,7 +15,7 @@ wget -q --no-check-certificate https://github.com/loris-imageserver/loris/raw/de
 chmod 755 kdu_expand
 
 # Python and image library dependencies
-apt-get install -qqy libjpeg8 libjpeg8-dev libfreetype6 libfreetype6-dev zlib1g-dev liblcms2-2 liblcms2-dev liblcms2-utils libtiff5-dev > /dev/null 2>&1
+apt-get install -qqy libjpeg8 libjpeg8-dev libfreetype6 libfreetype6-dev zlib1g-dev liblcms2-2 liblcms2-dev liblcms2-utils libtiff5-dev libffi-dev > /dev/null 2>&1
 ln -s /usr/lib/`uname -i`-linux-gnu/libfreetype.so /usr/lib/
 ln -s /usr/lib/`uname -i`-linux-gnu/libjpeg.so /usr/lib/
 ln -s /usr/lib/`uname -i`-linux-gnu/libz.so /usr/lib/
@@ -23,7 +23,7 @@ ln -s /usr/lib/`uname -i`-linux-gnu/liblcms.so /usr/lib/
 ln -s /usr/lib/`uname -i`-linux-gnu/libtiff.so /usr/lib/
 echo "/usr/local/lib" >> /etc/ld.so.conf && ldconfig
 apt-get install -qqy python-dev python-setuptools python-pip > /dev/null 2>&1
-pip install -qq --upgrade pip > /dev/null 2>&1
+pip install -qq --upgrade > /dev/null 2>&1
 pip uninstall -qq PIL > /dev/null 2>&1
 pip uninstall -qq Pillow > /dev/null 2>&1
 apt-get purge -qq python-imaging > /dev/null 2>&1
@@ -36,10 +36,11 @@ apt-get install -qqy libapache2-mod-wsgi > /dev/null 2>&1
 
 # Loris packages
 cd /opt
-wget -q --no-check-certificate https://github.com/loris-imageserver/loris/archive/2.0.1.zip
-unzip -qq 2.0.1.zip > /dev/null 2>&1
-mv loris-2.0.1/ loris/
-rm 2.0.1.zip
+wget -q --no-check-certificate https://github.com/loris-imageserver/loris/archive/v2.3.3.zip
+unzip -qq v2.3.3.zip > /dev/null 2>&1
+mv loris-2.3.3/ loris/
+rm v2.3.3.zip
+
 
 # Loris user
 useradd -d /var/www/loris2 -s /sbin/false loris
